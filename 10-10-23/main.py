e73ccee2cd1226a6ccbcae3a73d72865e329ee21
Ayush Thakur <100257355+ayushhhthakur@users.noreply.github.com>
2023-10-10 13:17:21 +0530

    Add files via upload

---
+++ 10-10-23/main.py
@@ -0,0 +1,10 @@
+'''
+take a string an perform the fpollowing string handeling function:
+1 find the lenghth of string
+2 convert the given string to lower case
+3 convert the given string to uppercase
+4 perform the slicing method to th egivcen string 
+5 find the index of any character from the given string 
+6 find the number count of any character in the given string 
+'''
+
