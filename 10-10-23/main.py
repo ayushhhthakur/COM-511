8a737ae4583d77a17b33268e06791fec83813e74
Ayush Thakur <100257355+ayushhhthakur@users.noreply.github.com>
2023-10-10 13:28:11 +0530

    Add files via upload

--- 10-10-23/main.py
+++ 10-10-23/main.py
@@ -8,3 +8,23 @@ take a string an perform the fpollowing string handeling function:
 6 find the number count of any character in the given string 
 '''
 
+input_str = "Hello My Name Is Ayush"
+find_char = 'a'
+
+length = len(input_str)
+print("1. Length of string is: ",length)
+
+lower = input_str.lower()
+print("2. String in Lowercase: ",lower)
+
+upper = input_str.upper()
+print("3. String in Uppercase: ",upper)
+
+slicied_string = input_str[3:6]
+print("4. Sliced string is: ", slicied_string)
+
+find_index = input_str.find(find_char)
+print(f"5. Index of '{find_char}' in the sting is: ", find_index)
+
+count_char = input_str.count(find_char)
+print(f"6. Count of '{find_char}' in the string is: ", count_char)
